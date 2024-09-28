3d0b626789bb3e4b08cc9eb8836af741bc67e1ef
Nathan876 <>
2024-09-28 13:56:04 +0200

    étape 3 : récupérer les infos

--- calculatrice/main.c
+++ calculatrice/main.c
@@ -6,5 +6,15 @@ int main()
  int premier_nombre;
  int second_nombre;
  char signe;
+ printf("Bienvenue dans la mini calculatrice !\n");
+ printf("Choisissez un opérateur (+, -, *, /, %%) :\n");
+ while (signe != '+' && signe != '-' && signe != '*' && signe != '/' && signe != '%')
+ {
+  scanf("%c", &signe);
+ }
+ printf("Choisissez un premier nombre :\n");
+ scanf("%d", &premier_nombre);
+ printf("Entrez un second nombre :\n");
+ scanf("%d", &second_nombre);;
  return 0;
 }
