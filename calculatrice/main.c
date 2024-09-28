f65cac0579fdc1efc128e80e7a811cd0b7a567b1
Nathan876 <>
2024-09-28 15:24:57 +0200

    étape 4 : créer les conditions

--- calculatrice/main.c
+++ calculatrice/main.c
@@ -7,14 +7,38 @@ int main()
  int second_nombre;
  char signe;
  printf("Bienvenue dans la mini calculatrice !\n");
- printf("Choisissez un opérateur (+, -, *, /, %%) :\n");
  while (signe != '+' && signe != '-' && signe != '*' && signe != '/' && signe != '%')
  {
-  scanf("%c", &signe);
+  printf("Choisissez un opérateur (+, -, *, /, %%) :\n"); 
+  scanf(" %c", &signe);
  }
  printf("Choisissez un premier nombre :\n");
  scanf("%d", &premier_nombre);
  printf("Entrez un second nombre :\n");
- scanf("%d", &second_nombre);;
+ scanf("%d", &second_nombre);
+ if (signe == '+')
+ {
+  printf("addition\n");
+ }
+ else if (signe == '-')
+ {
+  printf("soustraction\n");
+ }
+ else if (signe == '*')
+ {
+  printf("multiplication\n");
+ }
+ else if (signe == '/')
+ {
+  printf("division\n");
+ }
+ else if (signe == '%')
+ {
+  printf("modulo\n");
+ }
+ else
+ {
+  printf("Votre signe n'a pas été reconnu\n");
+ }
  return 0;
 }
